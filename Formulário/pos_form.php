af8d49b7fca787f886bf6c96a73f91a8cce1b349
Chapolin <167393532+ch4pol1n@users.noreply.github.com>
2024-09-02 22:47:47 -0300

    Update pos_form.php

--- Formulário/pos_form.php
+++ Formulário/pos_form.php
@@ -1,16 +1,7 @@
 <?php
-    $name = $_POST['mame'];
+    $name = $_POST['name'];
     $email = $_POST['email'];
+    
+    echo "<h3> bem vindo(a), $name";
 
-    $conn = new mysql("localhost:3306", "root", "", "formulario");
-    $query = "insert into dados(name, email) values ('$name','$email');";
-
-    $res = $conn->execute_query($list_query);
-
-    $list_query = ("select * from dados");
-    $list_query = $conn->execute_query($list_query);
-
-    echo "<h1>Usuarios cadastrados<h1>";
-    foreach ($list_res as $row) {
-        echo "<h2>" . $row["name"] . "<h2>";
-    }
+?>
